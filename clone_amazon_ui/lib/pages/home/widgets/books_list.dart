e09fa6314f0c89135cc85a913ac70ba26020e9b4
chewmunk <chw1787@gmail.com>
2022-09-07 23:36:18 +0900

    Add books_list.dart and SingleChildScrollView()

--- clone_amazon_ui/lib/pages/home/widgets/books_list.dart
+++ clone_amazon_ui/lib/pages/home/widgets/books_list.dart
@@ -1,5 +1,8 @@
 import 'package:clone_amazon_ui/constants.dart';
+import 'package:clone_amazon_ui/models/books_list.dart';
 import 'package:flutter/material.dart';
+
+final List<BookModel> books = BookModel.books;
 
 class BuildBookList extends StatelessWidget {
   @override
@@ -43,6 +46,62 @@ class BuildBookList extends StatelessWidget {
               ],
             ),
           ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: List.generate(
+                books.length,
+                (index) => Padding(
+                  padding: EdgeInsets.only(
+                    bottom: Constants.kPadding * 2,
+                    right: Constants.kPadding,
+                    left: index == 0 ? Constants.kPadding : 0,
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      //TODO
+                    },
+                    child: Column(
+                      children: [
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 4,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Image.asset(
+                              books[index].image,
+                              height: 180,
+                              width: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 120,
+                          child: Text(
+                            books[index].title,
+                          ),
+                        ),
+                        Container(
+                          width: 120,
+                          child: Text(
+                            books[index].subtitle,
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
